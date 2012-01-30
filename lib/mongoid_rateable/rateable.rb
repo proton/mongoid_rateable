e9190969edd781f9404b6d150632d4a1289eb7c5
Peter Savichev (proton) <psavichev@gmail.com>
2012-01-30 18:10:54 +0400

    updated dependencies, fixed "DEPRECATION WARNING: The InstanceMethods module inside ActiveSupport::Concern will be no longer included automatically."

--- lib/mongoid_rateable/rateable.rb
+++ lib/mongoid_rateable/rateable.rb
@@ -23,92 +23,88 @@ module Mongoid
  			scope :highest_rated, ->(limit=10) { order_by([:rating, :desc]).limit(limit) }
 		end
 
-		module InstanceMethods
+		def rate(mark, rater, weight = 1)
+			validate_rating!(mark)
+			unrate_without_rating_update(rater)
+			total_mark = mark.to_i*weight.to_i
+			self.rates += total_mark
+			self.rating_marks.new(:rater_id => rater.id, :mark => mark, :rater_class => rater.class.to_s, :weight => weight)
+			self.weighted_rate_count += weight
+			update_rating
+		end
 
-			def rate(mark, rater, weight = 1)
- 				validate_rating!(mark)
- 				unrate_without_rating_update(rater)
- 				total_mark = mark.to_i*weight.to_i
- 				self.rates += total_mark
- 				self.rating_marks.new(:rater_id => rater.id, :mark => mark, :rater_class => rater.class.to_s, :weight => weight)
-				self.weighted_rate_count += weight
- 				update_rating
+		def unrate(rater)
+			unrate_without_rating_update(rater)
+			update_rating
+		end
+
+		def rate_and_save(mark, rater, weight = 1)
+			rate(mark, rater, weight)
+			save
+		end
+
+		def unrate_and_save(rater)
+			unrate(rater)
+			save
+		end
+
+		def rated?(rater = nil)
+			if rater
+				puts "Deprecated method, please use rated_by?"
+				rated_by?(rater)
+			else
+				rate_count!=0
 			end
+		end
 
-			def unrate(rater)
-				unrate_without_rating_update(rater)
-				update_rating
+		def rated_by?(rater)
+			self.rating_marks.where(:rater_id => rater.id, :rater_class => rater.class.to_s).count == 1
+		end
+
+		def rating
+			read_attribute(:rating)
+		end
+
+		def rate_count
+			self.rating_marks.size
+		end
+
+		def rate_weight
+			check_weighted_rate_count
+			read_attribute(:weighted_rate_count)
+		end
+
+		protected
+
+		def validate_rating!(value)
+			if (defined? self.class::RATING_RANGE) and (range = self.class::RATING_RANGE) and !range.include?(value.to_i)
+				raise ArgumentError, "Rating not in range #{range}. Rating provided was #{value}."
 			end
+		end
 
-			def rate_and_save(mark, rater, weight = 1)
-				rate(mark, rater, weight)
-				save
+		def unrate_without_rating_update(rater)
+			rmark = self.rating_marks.where(:rater_id => rater.id, :rater_class => rater.class.to_s).first
+			if rmark
+				weight = (rmark.weight ||= 1)
+				total_mark = rmark.mark.to_i*weight.to_i
+				self.rates -= total_mark
+				self.weighted_rate_count -= weight
+				rmark.delete
 			end
+		end
 
-			def unrate_and_save(rater)
-				unrate(rater)
-				save
+		def update_rating
+			check_weighted_rate_count
+			rt = (self.rates.to_f / self.weighted_rate_count.to_f) unless self.rating_marks.blank?
+			write_attribute(:rating, rt)
+		end
+
+		def check_weighted_rate_count
+			#migration from old version
+			wrc = read_attribute(:weighted_rate_count).to_i
+			if (wrc==0 && rate_count!=0)
+				write_attribute(:weighted_rate_count, self.rating_marks.size)
 			end
-
-			def rated?(rater = nil)
-				if rater
-					puts "Deprecated method, please use rated_by?"
-					rated_by?(rater)
-				else
-					rate_count!=0
-				end
-			end
-
-			def rated_by?(rater)
-				self.rating_marks.where(:rater_id => rater.id, :rater_class => rater.class.to_s).count == 1
-			end
-
-			def rating
-				read_attribute(:rating)
-			end
-
-			def rate_count
-				self.rating_marks.size
-			end
-
-			def rate_weight
-				check_weighted_rate_count
-				read_attribute(:weighted_rate_count)
-			end
-
-			protected
-
-			def validate_rating!(value)
-				if (defined? self.class::RATING_RANGE) and (range = self.class::RATING_RANGE) and !range.include?(value.to_i)
-					raise ArgumentError, "Rating not in range #{range}. Rating provided was #{value}."
- 				end
-			end
-
-			def unrate_without_rating_update(rater)
-				rmark = self.rating_marks.where(:rater_id => rater.id, :rater_class => rater.class.to_s).first
-				if rmark
-					weight = (rmark.weight ||= 1)
-					total_mark = rmark.mark.to_i*weight.to_i
-					self.rates -= total_mark
-					self.weighted_rate_count -= weight
-					rmark.delete
-				end
-			end
-
-			def update_rating
-				check_weighted_rate_count
-				rt = (self.rates.to_f / self.weighted_rate_count.to_f) unless self.rating_marks.blank?
- 				write_attribute(:rating, rt)
-			end
-
-			def check_weighted_rate_count
-				#migration from old version
-				wrc = read_attribute(:weighted_rate_count).to_i
-				if (wrc==0 && rate_count!=0)
-					write_attribute(:weighted_rate_count, self.rating_marks.size)
-				end
-			end
-
 		end
 	end
 end
