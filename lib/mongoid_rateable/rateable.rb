85898b3dd7a1c74e1f34793fb7e7e72f2382e345
Alex Svirin <alexsvirin@gmail.com>
2012-07-23 17:33:43 +0300

    code reformatted

--- lib/mongoid_rateable/rateable.rb
+++ lib/mongoid_rateable/rateable.rb
@@ -1,15 +1,15 @@
 module Mongoid
-	module Rateable
-		extend ActiveSupport::Concern
+  module Rateable
+    extend ActiveSupport::Concern
 
-		included do
-			field :rates, :type => Integer, :default => 0
-			field :rating, :type => Float, :default => nil
-			field :weighted_rate_count, :type => Integer, :default => 0
+    included do
+      field :rates, :type => Integer, :default => 0
+      field :rating, :type => Float, :default => nil
+      field :weighted_rate_count, :type => Integer, :default => 0
 
-			embeds_many :rating_marks, :as => :rateable
+      embeds_many :rating_marks, :as => :rateable
 
-			if Mongoid::VERSION.to_f >= 3
+      if Mongoid::VERSION.to_f >= 3
         index({"rating_marks.rater_id" => 1, "rating_marks.rater_class" => 1})
       else
         index(
@@ -20,99 +20,99 @@ module Mongoid
         )
       end
 
-			scope :unrated, where(:rating.exists => false)
-			scope :rated, where(:rating.exists => true)
-			scope :rated_by, ->(rater) { where("rating_marks.rater_id" => rater.id, "rating_marks.rater_class" => rater.class.to_s) }
-			scope :with_rating, ->(range) { where(:rating.gte => range.begin, :rating.lte => range.end) }
- 			scope :highest_rated, ->(limit=10) { order_by([:rating, :desc]).limit(limit) }
-		end
+      scope :unrated, where(:rating.exists => false)
+      scope :rated, where(:rating.exists => true)
+      scope :rated_by, ->(rater) { where("rating_marks.rater_id" => rater.id, "rating_marks.rater_class" => rater.class.to_s) }
+      scope :with_rating, ->(range) { where(:rating.gte => range.begin, :rating.lte => range.end) }
+      scope :highest_rated, ->(limit=10) { order_by([:rating, :desc]).limit(limit) }
+    end
 
-		module InstanceMethods
+    module InstanceMethods
 
-			def rate(mark, rater, weight = 1)
- 				validate_rating!(mark)
- 				unrate_without_rating_update(rater)
- 				total_mark = mark.to_i*weight.to_i
- 				self.rates += total_mark
- 				self.rating_marks.new(:rater_id => rater.id, :mark => mark, :rater_class => rater.class.to_s, :weight => weight)
-				self.weighted_rate_count += weight
- 				update_rating
-			end
+      def rate(mark, rater, weight = 1)
+        validate_rating!(mark)
+        unrate_without_rating_update(rater)
+        total_mark = mark.to_i*weight.to_i
+        self.rates += total_mark
+        self.rating_marks.new(:rater_id => rater.id, :mark => mark, :rater_class => rater.class.to_s, :weight => weight)
+        self.weighted_rate_count += weight
+        update_rating
+      end
 
-			def unrate(rater)
-				unrate_without_rating_update(rater)
-				update_rating
-			end
+      def unrate(rater)
+        unrate_without_rating_update(rater)
+        update_rating
+      end
 
-			def rate_and_save(mark, rater, weight = 1)
-				rate(mark, rater, weight)
-				save
-			end
+      def rate_and_save(mark, rater, weight = 1)
+        rate(mark, rater, weight)
+        save
+      end
 
-			def unrate_and_save(rater)
-				unrate(rater)
-				save
-			end
+      def unrate_and_save(rater)
+        unrate(rater)
+        save
+      end
 
-			def rated?(rater = nil)
-				if rater
-					puts "Deprecated method, please use rated_by?"
-					rated_by?(rater)
-				else
-					rate_count!=0
-				end
-			end
+      def rated?(rater = nil)
+        if rater
+          puts "Deprecated method, please use rated_by?"
+          rated_by?(rater)
+        else
+          rate_count!=0
+        end
+      end
 
-			def rated_by?(rater)
-				self.rating_marks.where(:rater_id => rater.id, :rater_class => rater.class.to_s).count == 1
-			end
+      def rated_by?(rater)
+        self.rating_marks.where(:rater_id => rater.id, :rater_class => rater.class.to_s).count == 1
+      end
 
-			def rating
-				read_attribute(:rating)
-			end
+      def rating
+        read_attribute(:rating)
+      end
 
-			def rate_count
-				self.rating_marks.size
-			end
+      def rate_count
+        self.rating_marks.size
+      end
 
-			def rate_weight
-				check_weighted_rate_count
-				read_attribute(:weighted_rate_count)
-			end
+      def rate_weight
+        check_weighted_rate_count
+        read_attribute(:weighted_rate_count)
+      end
 
-			protected
+      protected
 
-			def validate_rating!(value)
-				if (defined? self.class::RATING_RANGE) and (range = self.class::RATING_RANGE) and !range.include?(value.to_i)
-					raise ArgumentError, "Rating not in range #{range}. Rating provided was #{value}."
- 				end
-			end
+      def validate_rating!(value)
+        if (defined? self.class::RATING_RANGE) and (range = self.class::RATING_RANGE) and !range.include?(value.to_i)
+          raise ArgumentError, "Rating not in range #{range}. Rating provided was #{value}."
+        end
+      end
 
-			def unrate_without_rating_update(rater)
-				rmark = self.rating_marks.where(:rater_id => rater.id, :rater_class => rater.class.to_s).first
-				if rmark
-					weight = (rmark.weight ||= 1)
-					total_mark = rmark.mark.to_i*weight.to_i
-					self.rates -= total_mark
-					self.weighted_rate_count -= weight
-					rmark.delete
-				end
-			end
+      def unrate_without_rating_update(rater)
+        rmark = self.rating_marks.where(:rater_id => rater.id, :rater_class => rater.class.to_s).first
+        if rmark
+          weight                   = (rmark.weight ||= 1)
+          total_mark               = rmark.mark.to_i*weight.to_i
+          self.rates               -= total_mark
+          self.weighted_rate_count -= weight
+          rmark.delete
+        end
+      end
 
-			def update_rating
-				check_weighted_rate_count
-				rt = (self.rates.to_f / self.weighted_rate_count.to_f) unless self.rating_marks.blank?
- 				write_attribute(:rating, rt)
-			end
+      def update_rating
+        check_weighted_rate_count
+        rt = (self.rates.to_f / self.weighted_rate_count.to_f) unless self.rating_marks.blank?
+        write_attribute(:rating, rt)
+      end
 
-			def check_weighted_rate_count
-				#migration from old version
-				wrc = read_attribute(:weighted_rate_count).to_i
-				if (wrc==0 && rate_count!=0)
-					write_attribute(:weighted_rate_count, self.rating_marks.size)
-				end
-			end
+      def check_weighted_rate_count
+        #migration from old version
+        wrc = read_attribute(:weighted_rate_count).to_i
+        if (wrc==0 && rate_count!=0)
+          write_attribute(:weighted_rate_count, self.rating_marks.size)
+        end
+      end
 
-		end
-	end
+    end
+  end
 end
--- lib/mongoid_rateable/rating.rb
+++ lib/mongoid_rateable/rating.rb
@@ -1,10 +1,10 @@
 class RatingMark
-	include Mongoid::Document
+  include Mongoid::Document
   include Mongoid::Timestamps::Created
 
-	embedded_in :rateable, :polymorphic => true
-	field :mark, :type => Integer
-	field :rater_class, :type => String
-	field :rater_id, :type => BSON::ObjectId
-	field :weight, :type => Integer, :default => 1
+  embedded_in :rateable, :polymorphic => true
+  field :mark, :type => Integer
+  field :rater_class, :type => String
+  field :rater_id, :type => Moped::BSON::ObjectId
+  field :weight, :type => Integer, :default => 1
 end
