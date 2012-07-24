cbb6abec043bf9ed36804ff4d5af88c92c1a53c6
Alex Svirin <alexsvirin@gmail.com>
2012-07-24 20:59:57 +0300

    removed jeweler, removed bson_ext dependency, removed depracations, updated gemspec, updated specs (due to mongoid/origin)

--- lib/mongoid_rateable/rateable.rb
+++ lib/mongoid_rateable/rateable.rb
@@ -9,16 +9,7 @@ module Mongoid
 
       embeds_many :rating_marks, :as => :rateable
 
-      if Mongoid::VERSION.to_f >= 3
-        index({"rating_marks.rater_id" => 1, "rating_marks.rater_class" => 1})
-      else
-        index(
-          [
-            ["rating_marks.rater_id", Mongo::ASCENDING],
-            ["rating_marks.rater_class", Mongo::ASCENDING]
-          ]
-        )
-      end
+      index({"rating_marks.rater_id" => 1, "rating_marks.rater_class" => 1})
 
       scope :unrated, where(:rating.exists => false)
       scope :rated, where(:rating.exists => true)
@@ -27,92 +18,83 @@ module Mongoid
       scope :highest_rated, ->(limit=10) { order_by([:rating, :desc]).limit(limit) }
     end
 
-    module InstanceMethods
+    def rate(mark, rater, weight = 1)
+      validate_rating!(mark)
+      unrate_without_rating_update(rater)
+      total_mark = mark.to_i*weight.to_i
+      self.rates += total_mark
+      self.rating_marks.new(:rater_id => rater.id, :mark => mark, :rater_class => rater.class.to_s, :weight => weight)
+      self.weighted_rate_count += weight
+      update_rating
+    end
 
-      def rate(mark, rater, weight = 1)
-        validate_rating!(mark)
-        unrate_without_rating_update(rater)
-        total_mark = mark.to_i*weight.to_i
-        self.rates += total_mark
-        self.rating_marks.new(:rater_id => rater.id, :mark => mark, :rater_class => rater.class.to_s, :weight => weight)
-        self.weighted_rate_count += weight
-        update_rating
+    def unrate(rater)
+      unrate_without_rating_update(rater)
+      update_rating
+    end
+
+    def rate_and_save(mark, rater, weight = 1)
+      rate(mark, rater, weight)
+      save
+    end
+
+    def unrate_and_save(rater)
+      unrate(rater)
+      save
+    end
+
+    def rated?
+      rate_count != 0
+    end
+
+    def rated_by?(rater)
+      self.rating_marks.where(:rater_id => rater.id, :rater_class => rater.class.to_s).count == 1
+    end
+
+    def rating
+      read_attribute(:rating)
+    end
+
+    def rate_count
+      self.rating_marks.size
+    end
+
+    def rate_weight
+      check_weighted_rate_count
+      read_attribute(:weighted_rate_count)
+    end
+
+    protected
+
+    def validate_rating!(value)
+      if (defined? self.class::RATING_RANGE) and (range = self.class::RATING_RANGE) and !range.include?(value.to_i)
+        raise ArgumentError, "Rating not in range #{range}. Rating provided was #{value}."
       end
+    end
 
-      def unrate(rater)
-        unrate_without_rating_update(rater)
-        update_rating
+    def unrate_without_rating_update(rater)
+      rmark = self.rating_marks.where(:rater_id => rater.id, :rater_class => rater.class.to_s).first
+      return unless rmark
+
+      weight                   = (rmark.weight ||= 1)
+      total_mark               = rmark.mark.to_i*weight.to_i
+      self.rates               -= total_mark
+      self.weighted_rate_count -= weight
+      rmark.delete
+    end
+
+    def update_rating
+      check_weighted_rate_count
+      rt = (self.rates.to_f / self.weighted_rate_count.to_f) unless self.rating_marks.blank?
+      write_attribute(:rating, rt)
+    end
+
+    def check_weighted_rate_count
+      #migration from old version
+      wrc = read_attribute(:weighted_rate_count).to_i
+      if (wrc==0 && rate_count!=0)
+        write_attribute(:weighted_rate_count, self.rating_marks.size)
       end
-
-      def rate_and_save(mark, rater, weight = 1)
-        rate(mark, rater, weight)
-        save
-      end
-
-      def unrate_and_save(rater)
-        unrate(rater)
-        save
-      end
-
-      def rated?(rater = nil)
-        if rater
-          puts "Deprecated method, please use rated_by?"
-          rated_by?(rater)
-        else
-          rate_count!=0
-        end
-      end
-
-      def rated_by?(rater)
-        self.rating_marks.where(:rater_id => rater.id, :rater_class => rater.class.to_s).count == 1
-      end
-
-      def rating
-        read_attribute(:rating)
-      end
-
-      def rate_count
-        self.rating_marks.size
-      end
-
-      def rate_weight
-        check_weighted_rate_count
-        read_attribute(:weighted_rate_count)
-      end
-
-      protected
-
-      def validate_rating!(value)
-        if (defined? self.class::RATING_RANGE) and (range = self.class::RATING_RANGE) and !range.include?(value.to_i)
-          raise ArgumentError, "Rating not in range #{range}. Rating provided was #{value}."
-        end
-      end
-
-      def unrate_without_rating_update(rater)
-        rmark = self.rating_marks.where(:rater_id => rater.id, :rater_class => rater.class.to_s).first
-        if rmark
-          weight                   = (rmark.weight ||= 1)
-          total_mark               = rmark.mark.to_i*weight.to_i
-          self.rates               -= total_mark
-          self.weighted_rate_count -= weight
-          rmark.delete
-        end
-      end
-
-      def update_rating
-        check_weighted_rate_count
-        rt = (self.rates.to_f / self.weighted_rate_count.to_f) unless self.rating_marks.blank?
-        write_attribute(:rating, rt)
-      end
-
-      def check_weighted_rate_count
-        #migration from old version
-        wrc = read_attribute(:weighted_rate_count).to_i
-        if (wrc==0 && rate_count!=0)
-          write_attribute(:weighted_rate_count, self.rating_marks.size)
-        end
-      end
-
     end
   end
 end
--- mongoid_rateable.gemspec
+++ mongoid_rateable.gemspec
@@ -46,30 +46,15 @@ Gem::Specification.new do |s|
   if s.respond_to? :specification_version then
     s.specification_version = 3
 
-    if Gem::Version.new(Gem::VERSION) >= Gem::Version.new('1.2.0') then
-      s.add_runtime_dependency(%q<bson_ext>, [">= 0"])
-      s.add_runtime_dependency(%q<mongoid>, [">= 0"])
-      s.add_development_dependency(%q<bundler>, ["~> 1.0.0"])
-      s.add_development_dependency(%q<jeweler>, ["~> 1.6.2"])
-      s.add_development_dependency(%q<simplecov>, [">= 0.4.0"])
-      s.add_development_dependency(%q<rdoc>, [">= 0"])
-      s.add_development_dependency(%q<rspec>, [">= 2.0.0"])
-      s.add_development_dependency(%q<database_cleaner>, [">= 0"])
-    else
-      s.add_dependency(%q<bson_ext>, [">= 0"])
-      s.add_dependency(%q<mongoid>, [">= 0"])
-      s.add_dependency(%q<bundler>, ["~> 1.0.0"])
-      s.add_dependency(%q<jeweler>, ["~> 1.6.2"])
-      s.add_dependency(%q<simplecov>, [">= 0.4.0"])
-      s.add_dependency(%q<rdoc>, [">= 0"])
-      s.add_dependency(%q<rspec>, [">= 2.0.0"])
-      s.add_dependency(%q<database_cleaner>, [">= 0"])
-    end
+    s.add_dependency(%q<mongoid>, [">= 3.0"])
+    s.add_dependency(%q<bundler>, ["~> 1.0.0"])
+    s.add_dependency(%q<simplecov>, [">= 0.4.0"])
+    s.add_dependency(%q<rdoc>, [">= 0"])
+    s.add_dependency(%q<rspec>, [">= 2.0.0"])
+    s.add_dependency(%q<database_cleaner>, [">= 0"])
   else
-    s.add_dependency(%q<bson_ext>, [">= 0"])
     s.add_dependency(%q<mongoid>, [">= 0"])
     s.add_dependency(%q<bundler>, ["~> 1.0.0"])
-    s.add_dependency(%q<jeweler>, ["~> 1.6.2"])
     s.add_dependency(%q<simplecov>, [">= 0.4.0"])
     s.add_dependency(%q<rdoc>, [">= 0"])
     s.add_dependency(%q<rspec>, [">= 2.0.0"])
